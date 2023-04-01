e0272c25aefad3bc79494b796bfc18607471ed56
Zak-Rey <zaktripple@gmail.com>
2023-04-01 08:09:06 +0300

    printing shape 7 java

--- PrintShape10.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class PrintShape10 {
-    List<ArrayList> arrList = {};
-
-}
---
+++ Shape8.java
@@ -0,0 +1,13 @@
+public class Shape8{
+    public static void main(String... args) {
+        String[] patterns = {
+            "    1111111  ",
+            "         11  ",
+            "        11   ",
+            "       11    ",
+            "      11     ",
+            "     11      ",
+            "    11        "
+        }
+    }
+}
