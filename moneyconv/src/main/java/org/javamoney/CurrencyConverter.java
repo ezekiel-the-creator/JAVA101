342e975e017b15531a2a0d65888ad321565fc97d
Zak-Rey <zaktripple@gmail.com>
2023-04-01 08:33:52 +0300

    currency converter java

--- moneyconv/src/main/java/org/javamoney/CurrencyConverter.java
+++ moneyconv/src/main/java/org/javamoney/CurrencyConverter.java
@@ -1,20 +1,17 @@
-package org.javamoney;
-import javax.money.CurrencyConverter;
+package java.javacash;
+
+import javax.money.CurrencyUnit;
 import javax.money.Monetary;
 import javax.money.MonetaryAmount;
 import javax.money.convert.CurrencyConversion;
 import javax.money.convert.MonetaryConversions;
 
-/**
- * converter
- *
- */
-public class CurrencyConverter{
+public class CurrencyConverter {
     
     public static double convert(String fromCurrencyCode, double amount, String toCurrencyCode){
         CurrencyUnit fromCurrency = Monetary.getCurrency(fromCurrencyCode);
         CurrencyUnit toCurrency = Monetary.getCurrency(toCurrencyCode);
-        MonetaryAmount fromAmount = Monetary.getDefaultAmountAmountFactory().setCurrency(fromCurrency).setNumber(amount).create();
+        MonetaryAmount fromAmount = Monetary.getDefaultAmountFactory().setCurrency(fromCurrency).setNumber(amount).create();
         CurrencyConversion conversion = MonetaryConversions.getConversion(toCurrencyCode);
         MonetaryAmount toAmount = fromAmount.with(conversion);
         return toAmount.getNumber().doubleValueExact();
