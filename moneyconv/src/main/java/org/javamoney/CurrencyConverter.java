98a4539004fcd8d40a768c39d02f27563d8e0a0c
Zak-Rey <zaktripple@gmail.com>
2023-04-02 06:07:58 +0300

    curency converter java

--- moneyconv/src/main/java/org/javamoney/CurrencyConverter.java
+++ moneyconv/src/main/java/org/javamoney/CurrencyConverter.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class CurrencyConverter {
     public static void main(String... args){
+        double exchangeRate;
+        double amount;
+        double convertedAmount;
+        Scanner scanner = new Scanner(System.in);
+
         
     }
 }
