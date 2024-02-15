1533d9155a9f409b43ae5a53cf4ce69b2801d3af
ezekiel-the-creator <zaktripple@gmail.com>
2024-02-15 07:43:22 +0300

    15

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args){
+        System.out.println("Command-Line arguments are");
+
+        for(String str: args) {
+            System.out.println(str);
+        }
+    }
+}
