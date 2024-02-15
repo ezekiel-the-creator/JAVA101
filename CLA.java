d7584026b0df4529edfabb2c8cdb6b549f0ea160
ezekiel-the-creator <zaktripple@gmail.com>
2024-02-15 07:42:55 +0300

    15

--- CLA.java
+++
@@ -1,9 +0,0 @@
-class CLA {
-    public static void main(String[] args){
-        System.out.println("Command-Line arguments are");
-
-        for(String str: args) {
-            System.out.println(str);
-        }
-    }
-}
